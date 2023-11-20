5603d9d5bd39bc292035a1de1a28b06efa2b7800
pwrwpw <dlgustjr9912@gmail.com>
2023-11-20 10:09:07 +0900

    chore: 프로젝트 초기 파일 설정

---
+++ src/main/java/com/blog/pwrwpw/PwrwpwApplication.java
@@ -0,0 +1,13 @@
+package com.blog.pwrwpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwrwpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwrwpwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/pwrwpw/PwrwpwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.pwrwpw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwrwpwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
