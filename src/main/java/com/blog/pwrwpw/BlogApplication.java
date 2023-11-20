1aa91dc6a171e81543621098109368aad4926e05
pwrwpw <dlgustjr9912@gmail.com>
2023-11-20 10:15:13 +0900

    chore: 프로젝트 명 수정

--- src/main/java/com/blog/pwrwpw/BlogApplication.java
+++ src/main/java/com/blog/pwrwpw/BlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PwrwpwApplication {
+public class BlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PwrwpwApplication.class, args);
+		SpringApplication.run(BlogApplication.class, args);
 	}
 
 }
